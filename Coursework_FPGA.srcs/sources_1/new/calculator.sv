88883792cb0a2a63f0f1faa0857ee7996f31c69f
karimnigmatulin <152773698+karimnigmatulin@users.noreply.github.com>
2024-03-16 18:02:07 +0500

    Added a calculator module

---
+++ Coursework_FPGA.srcs/sources_1/new/calculator.sv
@@ -0,0 +1,123 @@
+`timescale 1ns / 1ps
+
+module calculator(
+input         clock,
+    input         reset_n,
+    input         enter,
+    input         add,
+    input         multiply,
+    input   [7:0] data, 
+
+    output        newresult, 
+    output [15:0] result,
+    output        overflow,
+    output [3:0]  error
+);
+
+  assign error = 0;
+
+  reg  [15:0] alu_a;
+  reg  [15:0] alu_b;
+  reg         alu_multiply;
+  wire [15:0] alu_result;
+  wire        alu_overflow;
+
+  alu alu (
+      .a       (alu_a),
+      .b       (alu_b),
+      .multiply(alu_multiply),
+      .result  (alu_result),
+      .overflow(alu_overflow)
+  );
+
+  reg r_overflow;
+  assign overflow = r_overflow;
+
+  always @(posedge clock) begin
+    if (reset_n) r_overflow <= 0;
+    else       r_overflow <= alu_overflow;
+  end
+
+  reg         stack_push;
+  reg         stack_pop;
+  reg  [15:0] stack_write_data;
+  wire [15:0] stack_read_data;
+
+  stack stack (
+      .clock     (clock),
+      .reset_n   (reset_n),
+      .push      (stack_push),
+      .pop       (stack_pop),
+      .write_data(stack_write_data),
+      .read_data (stack_read_data)
+  );
+
+  reg [15:0] r_alu_a;
+  reg [15:0] r_alu_b;
+  reg        r_alu_multiply;
+  reg [1:0]  state;
+  reg [1:0]  next_state;
+  reg [15:0] oldresult = 0;
+  
+  reg temp_newresult;
+
+  assign result    = stack_read_data;
+//  assign newresult = (result != oldresult) && state == 0 && alu_a != 16'b0 && alu_b != 16'b0; // когда старый результат не равняется новому, мы находимся в нулевом состоянии - готовы принимать числа
+   assign newresult = (result != oldresult) && next_state == 0 && state == 2 && alu_a != 16'b0 && alu_b != 16'b0;
+   
+  always @(*) 
+  begin
+    alu_a               = r_alu_a;
+    alu_b               = r_alu_b;
+    alu_multiply        = r_alu_multiply;
+    stack_push          = 0;
+    stack_pop           = 0;
+    stack_write_data    = data;
+    next_state          = state;
+
+    case (state)
+        0:
+          if (enter) begin 
+            stack_push          = 1;
+            stack_write_data    = data;
+          end else 
+          if (add | multiply) begin
+            alu_a               = stack_read_data;
+            alu_multiply        = multiply;
+            stack_pop           = 1; 
+            next_state          = 1; 
+           
+          end
+    
+          1: begin 
+            alu_b               = stack_read_data;
+            stack_pop           = 1; 
+            next_state          = 2; 
+          end
+    
+          2: begin
+            stack_push          = 1; 
+            stack_write_data    = alu_result;
+            next_state          = 0;
+          end
+    endcase
+  
+  end
+
+  always @(posedge clock) begin 
+    if (reset_n) begin
+      r_alu_a           <= 0;
+      r_alu_b           <= 0;
+      r_alu_multiply    <= 0;
+      state             <= 0;
+      oldresult         <= 0;
+    end else begin
+      r_alu_a           <= alu_a;
+      r_alu_b           <= alu_b;
+      r_alu_multiply    <= alu_multiply;
+      state             <= next_state;
+      oldresult         <= result;
+    end
+  end
+  
+endmodule
